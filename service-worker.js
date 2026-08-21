2b9cf48d3d98c14812ad0f5d2ad6ff8e90088030
Lucasrx08 <lucasrigaux8@gmail.com>
2026-08-21 02:38:04 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='laser-run-v46-1-class-delete-20260820';
-const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='laser-run-v47-game-ui-20260821';
+const CORE=['./','./index.html','./theme-v47.css','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp)).catch(()=>{});return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
