6920fd66113f7704926b08cb400e6d9d63a01fe1
Lucasrx08 <lucasrigaux8@gmail.com>
2026-08-21 03:03:49 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='laser-run-v47-game-ui-20260821';
+const CACHE='laser-run-v47-1-display-fix-20260821';
 const CORE=['./','./index.html','./theme-v47.css','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
